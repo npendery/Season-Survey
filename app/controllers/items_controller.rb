57cf70997b309dc5b937571a930249f28e17ed10
Corrin Barnes <corrinbarnes@gmail.com>
2015-09-22 16:10:37 -0400

    updated @items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    if params[:category] == "food"
-      @items = Item.where(category_id: 1)
-    elsif params[:category] == "clothes"
-      @items = Item.where(category_id: 2)
-    else
-      @items = Item.where(category_id: 3)
-    end
+    @items = Item.where(Category.name == params[:category])
   end
 end

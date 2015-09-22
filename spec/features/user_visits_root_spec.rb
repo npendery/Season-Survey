62402abb37edd3bb433be574c6d03ebd4d9a9381
Jacob <jakegibs617@gmail.com>
2015-09-22 11:16:00 -0400

    Home Page Created hound 1

--- spec/features/user_visits_root_spec.rb
+++ spec/features/user_visits_root_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'user registers', %Q{
+feature 'user registers', %{
   As a visitor
   I want to visit the home page
   So that I can see a list of categories
@@ -10,13 +10,13 @@ feature 'user registers', %Q{
   # * Visitor must see link to sign in
   # * Visitor must see a list of categories that are links to other pages
 
-  scenario 'user visits root path' do
+	scenario 'user visits root path' do
 		item = FactoryGirl.create(:item)
 		category = FactoryGirl.create(:category)
 
-    visit root_path
-    expect(page).to have_content("Sign In")
-    expect(page).to have_content(category.name)
-    expect(page).to_not have_content(item.purchase_url)
-  end
+		visit root_path
+		expect(page).to have_content("Sign In")
+		expect(page).to have_content(category.name)
+		expect(page).to_not have_content(item.purchase_url)
+	end
 end

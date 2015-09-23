071a58178a95bbea0e97c497eb2955b81677369d
Corrin Barnes <corrinbarnes@gmail.com>
2015-09-23 14:43:59 -0400

    home page styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Category.create(name: "food")
-Category.create(name: "fun")
-Category.create(name: "clothes")
+Category.create(name: "food", image: "http://i.imgur.com/PZjSwig.jpg?1")
+Category.create(name: "fun", image: "http://i.imgur.com/N8TLfM3.jpg")
+Category.create(name: "fashion", image: "http://i.imgur.com/q3biltH.jpg")
 
 Item.create(name: "hot chocolate", image: "adkjf.png",
             category_id: 1, description: "amazazing",

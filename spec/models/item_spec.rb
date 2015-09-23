d0363ffb4eee1a49bf22f59915b0eea45b58a3fc
npendery <npendery@gmail.com>
2015-09-23 13:03:51 -0400

    second push submit w/updates 4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,14 +15,12 @@ RSpec.describe Item, type: :model do
     let!(:user) { FactoryGirl.create(:user) }
     let!(:category) { FactoryGirl.create(:category) }
     let!(:item) { FactoryGirl.create(:item, user: user, category: category) }
-    let!(:dupe_item) { FactoryGirl.create(:item,
-      user: user,
-      category: category) }
+    let!(:item_2) { FactoryGirl.create(:item, user: user, category: category) }
 
     it 'requires a unique name' do
       dupe_item.name = item.name
-      expect(dupe_item).to_not be_valid
-      expect(dupe_item.errors[:name]).to_not be_empty
+      expect(item_2).to_not be_valid
+      expect(item_2.errors[:name]).to_not be_empty
     end
   end
 end

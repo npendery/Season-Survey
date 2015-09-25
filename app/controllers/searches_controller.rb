8bf89c56800930cc4e3375a612b8d908662965b9
Jacob <jakegibs617@gmail.com>
2015-09-25 17:16:41 -0400

    searchbar added hound 2

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def index
-    @search_results = Item.search(params[:search]) || Review.search(params[:search]) 
+    @search_results = Item.search(params[:search]) || Review.search(params[:search])
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,10 +1,10 @@
 class Search < ActiveRecord::Base
-	def self.search(search)
-	  if search
-	    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-	    find(:all, :conditions => ['description LIKE ?', "%#{search}%"])
-	  else
-	    find(:all)
-	  end
-	end
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['description LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end

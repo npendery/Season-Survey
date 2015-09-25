a951a2cc75bcdbdfb3eac363db6c6cfbc6dec940
Jacob <jakegibs617@gmail.com>
2015-09-25 17:18:25 -0400

    searchbar added hound 4

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,6 @@
 class SearchesController < ApplicationController
   def index
-    @search_results = Item.search(params[:search]) || Review.search(params[:search])
+    @search_results = Item.search(params[:search]) ||
+                      Review.search(params[:search])
   end
 end

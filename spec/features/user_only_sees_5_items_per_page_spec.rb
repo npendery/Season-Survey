b43cf75edca20582d7679183070e756fe942a6aa
npendery <npendery@gmail.com>
2015-09-28 16:11:10 -0400

    edited for richard comments 2

--- spec/features/user_only_sees_5_items_per_page_spec.rb
+++ spec/features/user_only_sees_5_items_per_page_spec.rb
@@ -9,25 +9,25 @@ feature 'user sees only 5 items per page', %{
   scenario 'views category first page with the most recent items' do
     category = FactoryGirl.create(:category)
     user = FactoryGirl.create(:user)
-    item_list = FactoryGirl.create_list(:item, 7, user: user, category: category)
+    item_li = FactoryGirl.create_list(:item, 7, user: user, category: category)
 
     sign_in(user)
 
     click_on category.name
 
-    expect(page).to have_content(item_list[6].name)
-    expect(page).to have_content(item_list[5].name)
-    expect(page).to have_content(item_list[4].name)
-    expect(page).to have_content(item_list[3].name)
-    expect(page).to have_content(item_list[2].name)
-    expect(page).to_not have_content(item_list[1].name)
-    expect(page).to_not have_content(item_list[0].name)
+    expect(page).to have_content(item_li[6].name)
+    expect(page).to have_content(item_li[5].name)
+    expect(page).to have_content(item_li[4].name)
+    expect(page).to have_content(item_li[3].name)
+    expect(page).to have_content(item_li[2].name)
+    expect(page).to_not have_content(item_li[1].name)
+    expect(page).to_not have_content(item_li[0].name)
   end
 
   scenario 'views category second page with items after the first 5' do
     category = FactoryGirl.create(:category)
     user = FactoryGirl.create(:user)
-    item_list = FactoryGirl.create_list(:item, 7, user: user, category: category)
+    item_li = FactoryGirl.create_list(:item, 7, user: user, category: category)
 
     sign_in(user)
 
@@ -35,12 +35,12 @@ feature 'user sees only 5 items per page', %{
 
     click_on "Next"
 
-    expect(page).to have_content(item_list[1].name)
-    expect(page).to have_content(item_list[0].name)
-    expect(page).to_not have_content(item_list[6].name)
-    expect(page).to_not have_content(item_list[5].name)
-    expect(page).to_not have_content(item_list[4].name)
-    expect(page).to_not have_content(item_list[3].name)
-    expect(page).to_not have_content(item_list[2].name)
+    expect(page).to have_content(item_li[1].name)
+    expect(page).to have_content(item_li[0].name)
+    expect(page).to_not have_content(item_li[6].name)
+    expect(page).to_not have_content(item_li[5].name)
+    expect(page).to_not have_content(item_li[4].name)
+    expect(page).to_not have_content(item_li[3].name)
+    expect(page).to_not have_content(item_li[2].name)
   end
 end

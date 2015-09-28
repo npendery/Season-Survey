76f61018b03e76415df2862499cdd5d48a12fcb5
Jacob <jakegibs617@gmail.com>
2015-09-28 10:03:39 -0400

    test hound 4

--- spec/features/user_deletes_item_spec.rb
+++ spec/features/user_deletes_item_spec.rb
@@ -13,7 +13,9 @@ feature 'user deletes item', %{
     sign_in(user)
 
     click_on category.name
+
     click_on item.name
+
     click_on 'Delete Item'
 
     expect(page).to have_content("Item deleted.")
@@ -28,6 +30,7 @@ feature 'user deletes item', %{
     sign_in(user1)
 
     click_on category.name
+    
     click_on item.name
 
     expect(page).to_not have_content("Delete Item")

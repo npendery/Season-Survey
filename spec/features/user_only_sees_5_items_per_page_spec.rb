17fc36a7c9d413066253c9335632a5ccf7264f0a
npendery <npendery@gmail.com>
2015-09-28 16:09:41 -0400

    edited for richard comments

--- spec/features/user_only_sees_5_items_per_page_spec.rb
+++ spec/features/user_only_sees_5_items_per_page_spec.rb
@@ -9,59 +9,38 @@ feature 'user sees only 5 items per page', %{
   scenario 'views category first page with the most recent items' do
     category = FactoryGirl.create(:category)
     user = FactoryGirl.create(:user)
-    item1 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item2 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item3 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item4 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item5 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item6 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item7 = FactoryGirl.create(:item, user: user, category: category)
+    item_list = FactoryGirl.create_list(:item, 7, user: user, category: category)
+
     sign_in(user)
 
     click_on category.name
-    expect(page).to have_content(item7.name)
-    expect(page).to have_content(item6.name)
-    expect(page).to have_content(item5.name)
-    expect(page).to have_content(item4.name)
-    expect(page).to have_content(item3.name)
-    expect(page).to_not have_content(item1.name)
-    expect(page).to_not have_content(item2.name)
+
+    expect(page).to have_content(item_list[6].name)
+    expect(page).to have_content(item_list[5].name)
+    expect(page).to have_content(item_list[4].name)
+    expect(page).to have_content(item_list[3].name)
+    expect(page).to have_content(item_list[2].name)
+    expect(page).to_not have_content(item_list[1].name)
+    expect(page).to_not have_content(item_list[0].name)
   end
 
   scenario 'views category second page with items after the first 5' do
     category = FactoryGirl.create(:category)
     user = FactoryGirl.create(:user)
-    item1 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item2 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item3 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item4 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item5 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item6 = FactoryGirl.create(:item, user: user, category: category)
-    sleep 0.1
-    item7 = FactoryGirl.create(:item, user: user, category: category)
+    item_list = FactoryGirl.create_list(:item, 7, user: user, category: category)
+
     sign_in(user)
 
     click_on category.name
 
     click_on "Next"
 
-    expect(page).to have_content(item2.name)
-    expect(page).to have_content(item1.name)
-    expect(page).to_not have_content(item7.name)
-    expect(page).to_not have_content(item6.name)
-    expect(page).to_not have_content(item5.name)
-    expect(page).to_not have_content(item4.name)
-    expect(page).to_not have_content(item3.name)
+    expect(page).to have_content(item_list[1].name)
+    expect(page).to have_content(item_list[0].name)
+    expect(page).to_not have_content(item_list[6].name)
+    expect(page).to_not have_content(item_list[5].name)
+    expect(page).to_not have_content(item_list[4].name)
+    expect(page).to_not have_content(item_list[3].name)
+    expect(page).to_not have_content(item_list[2].name)
   end
 end

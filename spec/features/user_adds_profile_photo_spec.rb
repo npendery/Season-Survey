d63aef1dba7d9f0aaf15d5d48a13a291a49cadfd
Jacob <jakegibs617@gmail.com>
2015-09-29 13:00:01 -0400

    initial commit profile pic branch

--- spec/features/user_adds_profile_photo_spec.rb
+++ spec/features/user_adds_profile_photo_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 feature "user adds profile pic" do
   scenario "adds profile pic succcessfully" do
-	  user = FactoryGirl.create(:user)
-		visit root_path
-
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    visit root_path
     click_on "Profile"
 
     attach_file "Profile Photo", "#{Rails.root}/spec/support/images/example_photo.jpg"
-    click_on "Submit"
 
-    user = User.last
-    expect(page).to have_content("photo added")
+    fill_in "Current password", with: user.password
+    click_on "Update"
+
+    expect(page).to have_content("Your account has been updated successfully.")
     expect(page).to have_xpath("//img[@src=\"/uploads/user/profile_photo/#{user.id}/example_photo.jpg\"]")
-    expect(user.profile_photo.file.filename).to eq("example_photo.jpg")
   end
 end

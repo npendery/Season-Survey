500e86183fa757c7b17dbda4f959a85ad8b1eafb
Jacob <jakegibs617@gmail.com>
2015-10-01 12:40:26 -0400

    css round one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :profile_photo
     devise_parameter_sanitizer.for(:sign_up) << :profile_photo
+    devise_parameter_sanitizer.for(:account_update) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 end

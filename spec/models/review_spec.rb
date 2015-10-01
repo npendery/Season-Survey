806dfccdf3d5c5f9a346655dc1db7a322bbc9f68
Corrin Barnes <corrinbarnes@gmail.com>
2015-10-01 14:23:02 -0400

    model test on review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Review, type: :model do
 
   it { should have_valid(:description).when('this is really cool stuff') }
   it { should_not have_valid(:description).when('', nil) }
+
+  describe "#total_score" do
+    it "it sums the score for each vote" do
+      review = FactoryGirl.create(:review)
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      vote1 = FactoryGirl.create(:vote, user: user1, review: review)
+      vote2 = FactoryGirl.create(:vote, user: user2, review: review)
+
+      expect(review.total_score).to eq(2)
+    end
+  end
 end
